43c53a1a51f23795f1db2cdf5fe66c65ef7ad881
Felix d'Arco <darco.felix44@gmail.com>
2025-07-10 19:30:30 +0200

    generation de profil possible meme apres apres  avoir fait make up normal +protection mdp au moins 1chiffre, 8carac et majuscule

--- backend/seed/fakeProfilsIfEmpty.js
+++ backend/seed/fakeProfilsIfEmpty.js
@@ -5,20 +5,10 @@ const seedFakeProfils = require('./fakeProfils');
 (async () => {
     try {
         if (process.env.SEED_FAKE_PROFILES !== 'true') {
-        console.log("⏩ Skipping fake profile generation (SEED_FAKE_PROFILES is false)");
-        process.exit(0);
+            console.log("⏩ Skipping fake profile generation (SEED_FAKE_PROFILES is false)");
+            process.exit(0);
         }
-
-        const {rows} = await pool.query('SELECT COUNT(*) FROM users');
-        const count = parseInt(rows[0].count,10);
-
-        if (count === 0) {
-            console.log('✅ Base vide : insertion de 500 profils factices...');
-            await seedFakeProfils();
-            console.log('✅ 500 utilisateurs générés avec succès.');
-        } else {
-            console.log(`ℹ️ La base contient déjà ${count} utilisateurs. Aucun seed effectué.`);
-        }
+        await seedFakeProfils();
     } catch (error) {
         console.error("erreur lors du seed:", error);
     } finally {
